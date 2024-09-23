3d036e6624430e371b4922d3a9534a0d63d64f87
yishain11 <yishain11@walla.co.il>
2024-09-23 12:44:19 +0300

    added more functionality

--- game.js
+++ game.js
@@ -1,6 +1,90 @@
-class Game { }
+const readline = require('node:readline');
 
-class Player { }
+class Game {
+    constructor() {
+        this.deck = new Deck();
+        this.players = [];
+        this.isGameRunning = true;
+        this.pile = new Pile();
+        this.generatePlayers();
+        this.dealCards();
+        this.currentPlayerTurn = 0;
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        this.playRound();
+    }
+
+    generatePlayers() {
+        for (let i = 1; i < 3; i++) {
+            this.players.push(new Player(i));
+            // const rl = readline.createInterface({
+            //     input: process.stdin,
+            //     output: process.stdout,
+            // });
+            // rl.question(`What's player ${i}?`, name => {
+            //     rl.close();
+            // });
+        }
+    }
+
+    dealCards() {
+        for (let i = 0; i < this.players.length; i++) {
+            const player = this.players[i];
+            for (let i = 0; i < 4; i++) {
+                const card = this.deck.cards.shift();
+                player.addCard(card);
+            }
+        }
+    }
+
+    playRound() {
+        while (this.isGameRunning) {
+            console.log(`player ${this.currentPlayerTurn}: its your turn `);
+            const actions = ["0: draw from deck", "1: take from pile"];
+            let action;
+            while (action === undefined) {
+                this.rl.question(`What is you action? ${JSON.stringify(actions)}. 0 or 1 `, action => {
+                    switch (action) {
+                        case '0':
+                            action = 0;
+                            break;
+                        case '1':
+                            if (this.pile.length <= 0) {
+                                console.log("cannot draw from pile. pile is empty");
+                                break;
+                            }
+                            action = 1;
+                            const cardFromPile = this.pile.pop();
+                            console.log(`draw card from pile: ${cardFromPile}`);
+                            rl.question(`which card in your hand will you like to switch? 0-3`, (answer) => {
+                                if (parseInt(answer) < 0 || parseInt(answer) > 4) {
+                                    const cardFromHand = this.players[this.currentPlayerTurn].handOfCards[parseInt(answer)];
+                                    this.players[this.currentPlayerTurn].handOfCards[parseInt(answer)] = cardFromPile;
+                                    this.pile.push(cardFromHand);
+                                }
+                            });
+                            break;
+                    }
+                    this.rl.close();
+                });
+            }
+        }
+        this.currentPlayerTurn !== 0 ? this.currentPlayerTurn = 0 : this.currentPlayerTurn = 1;
+    }
+}
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.handOfCards = [];
+        this.score = 0;
+    }
+    addCard(card) {
+        this.handOfCards.push(card);
+    }
+}
 
 class Deck {
     constructor() {
@@ -13,7 +97,8 @@ class Deck {
         const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
         values.forEach((value) => {
             suites.forEach((suite) => {
-                this.cards.push(`${value}${suite}`);
+                const card = new Card(suite, value);
+                this.cards.push(card);
             });
         });
     }
@@ -32,9 +117,6 @@ class Deck {
     }
 
 }
-const d1 = new Deck();
-console.log('✌️d1 --->', d1);
-
 class Pile {
     constructor() {
         this.cards = [];
@@ -47,3 +129,4 @@ class Card {
         this.value = value;
     };
 }
+const game = new Game();
